d30a63211648a2b50675c755f27c70bfcbfb4c30
Noah Hamann <njhamann@gmail.com>
2013-05-02 17:38:50 -0400

    getting things organized

--- jobs/scraper.js
+++ jobs/scraper.js
@@ -35,30 +35,37 @@ function getPage(){
 
 // parse functions
 function parseJSON(json){
-    var obj = JSON.parse(json);
-    data = objKeysToUnderscore(obj);
-    data.time_saved = moment().unix();
-    console.log(data);
-    insertData(data, 'raw_notes_summary');
-};
-
-function parseDom(body){
-    jsdom.env(body, ["http://code.jquery.com/jquery.js"], extractData); 
-};
-
-function extractData(err, window){
-    var $ = window.$;
-    var rawData = {};
-    rawData.net_annualized_return = parseFloat($.trim($('.main-module .box-module:eq(0) label:eq(1) span').text()).replace(/[^0-9\.]+/g, ''));
-    rawData.interest_received = parseFloat($.trim($('.main-module .box-module:eq(1) label:eq(1) span').text()).replace(/[^0-9\.]+/g, ''));
-    rawData.account_total = parseFloat($.trim($('.main-module .box-module:eq(2) label:eq(1) span').text()).replace(/[^0-9\.]+/g, ''));
+    var rawData = JSON.parse(json);
+    rawData = objKeysToUnderscore(rawData);
     rawData.time_saved = moment().unix();
-    console.log(rawData);
-    insertData(rawData, 'raw_top_stats');
+    rawData = cleanData(rawData);
+    console.log(rawData); 
+    insertData(rawData, 'raw_notes_summary');
     toTimeseriesData(rawData, 'day_interval_top_stats', insertData);
 };
 
+function parseDom(body){
+    jsdom.env(body, ["http://code.jquery.com/jquery.js"], function(err, window){
+        var rawData = extractData(window);
+        rawData = cleanData(rawData);
+        
+        insertData(rawData, 'raw_top_stats');
+        toTimeseriesData(rawData, 'day_interval_top_stats', insertData);
+    }); 
+};
+
+function extractData(window){
+    var $ = window.$;
+    var rawData = {};
+    rawData.net_annualized_return = $.trim( $('.main-module .box-module:eq(0) label:eq(1) span').text() );
+    rawData.interest_received = $.trim( $('.main-module .box-module:eq(1) label:eq(1) span').text() );
+    rawData.account_total = $.trim( $('.main-module .box-module:eq(2) label:eq(1) span').text() );
+    rawData.time_saved = moment().unix();
+    return rawData;
+};
+
 function toTimeseriesData(data, collectionName, callback){
+
     /*
         format:
 
@@ -73,15 +80,11 @@ function toTimeseriesData(data, collectionName, callback){
                 count: [value]
             }
         }
-    
-        mongodb notes:
-        db.collection.find( { field: { $gt: value1, $lt: value2 } } );
 
     */
 
     var today_unix = moment().startOf('day').unix();
-    var yesterday_unix = moment.unix(today).subtract('days', 1).unix();
-    var data = {};
+    var yesterday_unix = moment.unix(today_unix).subtract('days', 1).unix();
     
     function convertData(last, curr){
         var d = {};
@@ -95,6 +98,23 @@ function toTimeseriesData(data, collectionName, callback){
         return d;
     };
     
+    var oldData = { charged_off: 4,
+      charged_off_amount: 40,
+      default_l: 0,
+      default_l_amount: 0,
+      fully_paid: 11,
+      fully_paid_amount: 300,
+      in_funding: 119,
+      in_funding_amount: 2975,
+      issued_and_current: 690,
+      issued_and_current_amount: 17558,
+      late16to30_amount: 0,
+      late31to120_amount: 123,
+      time_saved: 1367526616 }; 
+    
+    var converted = convertData(oldData, data);
+    console.log(converted);
+    return;
     // get last timeseries
     var server = new mongodb.Server("127.0.0.1", 27017, {});
     new mongodb.Db('lending_club', server, {w: 1}).open(function (error, client) {
@@ -160,4 +180,13 @@ function objKeysToUnderscore(obj){
         delete obj[key];
     }
     return obj;
+}
+
+function cleanData(data){
+    for(var key in data){
+        if(typeof data[key] == 'string'){
+            data[key] = parseFloat( data[key].replace( /[^0-9\.]+/g, '' ) );
+        }
+    }; 
+    return data;
 }
